92c8a89df6bb29d6c9acb84ddfea77781ed5cb43
onadeulofeu <ona.deulofeu@gmail.com>
2023-11-24 18:12:58 +0100

    add a shadow for the sd of the mean ploted

--- src/calculate_and_plot_residuals.R
+++ src/calculate_and_plot_residuals.R
@@ -22,22 +22,33 @@ plot_residuals <- function(data_anom_abund, data_mean_abund_asv, asv_num, commun
   
   community_fraction <- match.arg(community_fraction, c('0.2', '3')) # in case the fraction don't match this values an error will appear
   
+  data_mean_abund_f <- data_mean_abund_asv |>
+    dplyr::filter(asv_num == {{asv_num}} &
+                    fraction == {{community_fraction}})
+    
   plot_residuals <- data_anom_abund |>
     left_join(data_mean_abund_asv, by = c('asv_num', 'fraction')) |>
     dplyr::mutate(residual = abundance_value - mean_abund) |>
     dplyr::filter(asv_num == {{asv_num}} & 
                     fraction == {{community_fraction}}) |>
     ggplot(aes(date, residual))+
+    geom_rect(data = timeseries_limits, mapping = aes(xmin = date_min, xmax = date_max,  x=NULL, y=NULL,
+                                                      ymin = (data_mean_abund_f$mean_abund - data_mean_abund_f$sd_abund), 
+                                                      ymax = (data_mean_abund_f$mean_abund + data_mean_abund_f$sd_abund)),
+              fill = '#94969E',
+              alpha = 0.6)+
+    geom_hline(yintercept = data_mean_abund_f$mean_abund)+
     geom_point()+
-    geom_hline(yintercept = mean_abund_f_asv_asv_1$mean_abund)+
-    geom_segment(aes(x = date, y = residual, xend = date, yend = mean_abund_f_asv_asv_1$mean_abund), linetype = "dashed")+ 
+    geom_segment(aes(x = date, y = residual, xend = date, yend = data_mean_abund_f$mean_abund), linetype = "dashed")+ 
+   
     labs(title = {{asv_num}})+
-    scale_x_datetime(date_labels = "%Y", limits = c(min(data_anom_abund$date), max(data_anom_abund$date)))+
-    labs(x = 'Time', y = 'Difference from mean abundance during\n potential blooming event')+
+    scale_x_datetime(date_labels = "%Y", limits = c(min(timeseries_limits$date_min), max(timeseries_limits$date_max)))+
+    labs(x = 'Time', y = 'Difference between\ngeometric mean\nabundance during\n a potential blooming\nevent')+
     theme_bw()+
     theme(panel.grid = element_blank(),
           strip.background = element_blank(),
           text = element_text(size = 5),
+          axis.title.y = element_text(size = 3),
           legend.key.size = unit(4, 'mm'))
   
   return(plot_residuals)
