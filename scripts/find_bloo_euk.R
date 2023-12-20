8c443805399156d9f4f5a635270ea27fe040f4fa
onadeulofeu <ona.deulofeu@gmail.com>
2023-12-20 11:27:42 +0100

    add raw samples from the nano fraction to the analysis

--- scripts/find_bloo_euk.R
+++ scripts/find_bloo_euk.R
@@ -55,7 +55,6 @@ samples_raw_18 <- euk_raw |>
   dplyr::mutate(n = n()) |>
   dplyr::filter(n >= 3)
 
-
 ## I try to find blooms in the data that Ramon considered in his previous work and see if we observe blooms or not in eukaryotes -----
 euk_def
 
@@ -80,7 +79,7 @@ m_euk_def |>
 
 colnames(euk_def_l) <- c('asv_num', "sample_id", 'reads')
 
-colnames(tax_bbmo_10y_old) <- c("asv_num",'seq', 'length', 'group', 'supergroup', 'match_eukV4',
+colnames(tax_euk_def) <- c("asv_num",'seq', 'length', 'group', 'supergroup', 'match_eukV4',
                                 'pident_eukV4', 'query_eukV4', "genus", 'species', 'pident_species',
                                 "query_species", 'AN', "strain", "complete_name", "MAST_clade",
                                 'MAST_ref', 'pident_MAST', 'fungi', 'occurrence', 'reads', 'pr2match',
@@ -91,7 +90,6 @@ colnames(m_euk_def) <- c("sample_id", "original_name", "project", "station",
                           "depth_zone", "fraction", "extract", "temperature",       
                           "chla", "proj", "st", "dep",        
                           "frac", "ext", "classified")
-
 
 ## Divide metadata into FL and PA----
 m_02 <- m_euk_def  |>
@@ -129,7 +127,6 @@ m_02 |>
   summarize(n = n()) |>
   dplyr::filter(n < 12) #they kept all samples from the small fraction
 
-
 ## I recover this group of samples from the 'raw' dataset
 ##check which samples were discarded and which where kept from this dataset. Try to recover those that were not considered.
 euk_def_sam_data$Original_name
@@ -144,27 +141,64 @@ euk_raw_rel_tax <- euk_raw |>
   calculate_rel_abund(group_cols = sample_id) |>
   dplyr::filter(str_detect(sample_id, '_nano_')) |> # we want to compare only the nano fraction
   left_join(tax_euk_raw, by = 'ASV') |>
-  separate(sample_id, into = c('sample_id_code', 'fraction', 'replicate'), remove = F)
-
-euk_raw_rel_tax |>
- 
-
-euk_raw_rel_tax$sample_id
-
-euk_raw_rel_tax |>
+  separate(sample_id, into = c('sample_id_code', 'fraction', 'replicate'), remove = F) |>
   ungroup() |>
   dplyr::mutate(
     project = substr(sample_id_code, 1, 2),
     year = substr(sample_id_code, 3, 4),
     month = substr(sample_id_code, 5, 6),
     day = substr(sample_id_code, 7, 8),
-    date = paste(day, '-', month, '-', year)
-  ) |>
-  ggplot(aes(date, as.numeric(relative_abundance), fill = Supergroup))+
-  facet_wrap(vars(replicate))
+    date = paste0(day,'-',month,'-',year, sep = ''))
+
+ euk_raw_rel_tax$sample_id
+ 
+euk_raw_rel_tax |>
+  ggplot(aes(replicate, as.numeric(relative_abundance), fill = Supergroup))+
+  geom_col()+
+  facet_wrap(vars(date)) ##we observe if hte replicates are different between them or similar. Is seems that they are mainly equal. 
+##I'll keep r3 in case there are two of them. 
+
+euk_raw_rel_tax_rep <- euk_raw_rel_tax |>
+  group_by(date) |>
+  dplyr::filter(case_when(
+    'r3' %in% replicate ~ replicate == 'r3',
+    'r2' %in% replicate ~ replicate == 'r2',
+    'r1' %in% replicate ~ replicate == 'r1',
+    TRUE ~ n() == 1  # Keep if there's only one replicate
+  ))
+
+# 
+# samples_raw_18_nano <- samples_raw_18 |>
+#   dplyr::filter(fraction == 'nano')
+
+# I'll add these samples to the general dataset and observe what do we see in the eukaryotic commmunity. Do we trust these samples?
+## I need the same columns to add the samples that were eliminated to the dataset
+euk_raw_rel_tax_rep |>
+  colnames()
+
+euk_raw_rel_tax_rep %$%
+  sample_id
+
+euk_def_l_rel %$%
+  sample_id
+
+euk_def_l_rel <- euk_def_l |>
+  calculate_rel_abund(group_cols = sample_id) |>
+  left_join(tax_euk_def, by = 'asv_num') |>
+  ungroup() |>
+  dplyr::mutate(
+    project = substr(sample_id, 1, 3),
+    year = substr(sample_id, 5, 6),
+    month = substr(sample_id, 7, 8),
+    day = substr(sample_id, 9, 10),
+    date = paste0(day,'-',month,'-',year, sep = ''))
+
+euk_def_l_rel |>
+  colnames()
   
-samples_raw_18_nano <- samples_raw_18 |>
-  dplyr::filter(fraction == 'nano')
-
-
-
+euk_def_l_rel_f <- euk_def_l_rel |>
+  dplyr::select(date, year, month, day, relative_abundance, asv_num, group, supergroup, sample_id)
+  
+euk_raw_rel_tax_rep |>
+  dplyr::select()
+  
