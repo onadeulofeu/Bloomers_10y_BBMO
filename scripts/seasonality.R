ed1707c4314222f6687ead982fd8566ec4a52f3d
onadeulofeu <ona.deulofeu@gmail.com>
2024-01-23 14:57:28 +0100

    add a check for constant sampling before performing the analysis

--- scripts/seasonality.R
+++ scripts/seasonality.R
@@ -7,6 +7,7 @@ library(ggthemes)
 library(stringr)
 library(tidyverse)
 library(phyloseq)
+library(ggpubr)
 
 # Differentiate seasonal from non-seasonal bloomers to analyze them separately----
 
@@ -175,7 +176,32 @@ asv_tab_all_bloo_z_tax_02 #FL dataset
 
 ##(need to be re-calcualted for the PA fraction!!! )
 
+## Check sampling frequency----
+## In timeseries analysis whether if we perform Fourier analysis or Wavelets they assume constant sampling. We check it before we go any further.
+###check normality before correlation
+shapiro.test(as.numeric(sampling_freq$real_sampling)) # => 0.0004153 (NO-NORMALITY)
+ggqqplot(as.numeric(sampling_freq$real_sampling))
 
+shapiro.test(as.numeric(perfect_sampling_freq$ideal_sampling)) #p-value = 0.0004823 (NO-NORMALITY)
+ggqqplot(as.numeric(perfect_sampling_freq$ideal_sampling))
+
+check_sampling_constant <-  sampling_freq |>
+  bind_cols(perfect_sampling_freq) |>
+  ggplot(aes(real_sampling, ideal_sampling))+
+  geom_smooth(method = 'lm', color = 'black', alpha = 0.3)+
+  geom_point(alpha = 0.6)+
+  scale_x_continuous(expand = c(0,0))+
+  scale_y_continuous(expand = c(0,0))+
+  stat_cor(aes(label = paste(..p.label..)), label.x = 2006,
+           p.digits = 0.01, digits = 2, p.accuracy = 0.01, method = 'spearman')+
+  labs(x = 'Real sampling', y = 'Ideal sampling')+
+  theme_bw()+
+  theme(panel.grid = element_blank(), text = element_text(size = 5),
+        axis.ticks.length = unit(.25, "mm"))
+
+# ggsave(check_sampling_constant,  filename = 'check_sampling_frequency.pdf',
+#        path = 'Results/Figures/',
+#        width = 88, height = 88, units = 'mm')
 
 ## Adria's functions
 ## https://gitlab.com/aauladell/AAP_time_series here we have the source.
@@ -227,6 +253,7 @@ four <- asv_tab_all_bloo_z_tax_02 |>
   arrange(decimal_date)
 
 ## Seasonality of each ASV that we believe can be a potential bloomer
+
 ## I divide the different abundance values in separated plots and I filter only the potential bloomers in FL or PA-----
 bloo_02 <- read.csv('data/bloo_02.csv') |>
   as_tibble()
@@ -385,12 +412,14 @@ asv178 |>
 abundance_values <- asv11$abundance_value
 abundance_values <- asv178$abundance_value
 
-# Access the wavelet coefficients for the first scale
-coefficients_scale1 <- modwt_result$d4
+ibmr.la8 <- modwt(abundance_values, n.levels = 4, wf = "la8")
+names(ibmr.la8) <- c("w1", "w2", "w3", "w4", "v4")
 
+# Access the wavelet coefficients for the first scale
+coefficients_scale1 <- ibmr.la8$d4
 
 # Plot the wavelet coefficients for the first scale
-plot(coefficients_scale1, type = "l", col = "blue", xlab = "Index", ylab = "Wavelet Coefficients", main = "Wavelet Coefficients - Scale 1")
+plot(coefficients_scale1, type = "l", col = "blue", xlab = "Decimal date", ylab = "Wavelet Coefficients", main = "Wavelet Coefficients - Scale 1")
 
 # Reconstruct the signal from the wavelet coefficients and scaling coefficients
 reconstructed_signal <- imodwt(modwt_result)
@@ -422,5 +451,295 @@ for(i in 1:5)
 axis(side=1, at=seq(0,368,by=23), 
      labels=c(0,"",46,"",92,"",138,"",184,"",230,"",276,"",322,"",368))
 
+# Here I try to apply the code that is in apendix A of the thesis -----
+
+### Wavelets assume constant a constant sampling interval
+### How constant is my timeseries data?
+## MODWT (Maximal overlap discrete wavelet transforms):
+
+# abundance vector. 
+abund11 <- asv11$abundance_value
+modwtasv11 <- modwt(abund11, wf = 'la8', boundary = "periodic", n.levels = 4) |>
+  brick.wall(wf = 'la8') |> #elimination of all boundary coefficients is accomplished by the function 'brick.wall' prior to the phase shift correction
+  phase.shift(wf = 'la8') # this correction is necessary because the filtering with wavelet filter induces a displacement of features relative to the original time series.
+  
+modwtasv11_biased <- modwt(abund11, wf = 'la8', boundary = "periodic", n.levels = 4) |>
+  #brick.wall(wf = 'la8') |> #elimination of all boundary coefficients is accomplished by the function 'brick.wall' prior to the phase shift correction
+  phase.shift(wf = 'la8') # this correction is necessary because the filtering with wavelet filter induces a displacement of features relative to the original time series.
+
+abund178 <- asv178$abundance_value
+modwtasv178 <- modwt(abund178, wf = 'la8', boundary = "periodic", n.levels = 4) |>
+  brick.wall(wf = 'la8') |> #elimination of all boundary coefficients is accomplished by the function 'brick.wall' prior to the phase shift correction
+  phase.shift(wf = 'la8') 
+
+modwtasv178_biased <- modwt(abund178, wf = 'la8', boundary = "periodic", n.levels = 4) |>
+  #brick.wall(wf = 'la8') |> #elimination of all boundary coefficients is accomplished by the function 'brick.wall' prior to the phase shift correction
+  phase.shift(wf = 'la8') # this correction is necessary because the filtering with wavelet filter induces a displacement of features relative to the original time series.
+
+# Continue with phase shift
+shifted_result <- phase.shift(brick.wall(result, filter = db5_filter), filter = db5_filter)
+
+# e-folding estimates
+x <- rep(0, 10001) 
+x[5000] <- 1 
+n.levels <- 4 
+len <- length(abund) 
+temp <- phase.shift(modwt(x, n.levels = n.levels, wf = "la8"), wf = "la8")
+
+ waveExtremes <- matrix(nrow = 3, ncol = n.levels + 1) 
+ colnames(waveExtremes) <- c(paste("d", 1:n.levels, sep = ""), paste("s", n.levels,  sep = "")) 
+ rownames(waveExtremes) <- c("left", "right", "top")
+
+  for (i in 1:(n.levels + 1)) waveExtremes[, i] <- c(range(which(abs(temp[[i]]) 
+                                                                 >= max(abs(temp[[i]]))/(exp(2)))), which.max(abs(temp[[i]])))
+
+ boundaries <- data.frame(end = len - (waveExtremes[3, ] - waveExtremes[1, ]), 
+                          start = waveExtremes[2, ] - waveExtremes[3, ])
+ 
+ for (j in 1:(n.levels + 1)) { 
+   is.na(modwtasv11[[i]]) <- c(1:boundaries$start[i], boundaries$end[i]:length(modwtasv11[[i]])) 
+   is.na(modwtasv178[[i]]) <- c(1:boundaries$start[i], boundaries$end[i]:length(modwtasv178[[i]])) 
+   }
+ 
+ ##PLOTS----
+ #### Interpretation of the following plots
+ #### MODWTs of CLR transformation of the relative abundance from the potential blooming ASVs. The series d1-d4 are wavelet coefficient
+ ### vectors; the series s4 is the scaling coefficient vector at the coarsest scale. Coefficients not enclosed by the red solid lines are
+ ### boundary coefficients (coefficients affected by circular filtering). The blue dashed lines are delineate the filter-specific e-folding
+ ### distances from the beginning and end of the series (cone-of-influence in Torrence and Compo, 1998).
+ 
+ ### d1 is the finest scale and d4 is the coarsest. The s4 depicts the scaling coefficients as a function of time, representing the remainder
+ ### of the variability after the finder sacles (d1-d4) have been isolated. 
+ 
+ ### The inner red, solid, vertical lines represent the limits of the boundary coefficients. The regions enclosed by either set of limits become
+ ### progressively smaller as wavelet filters become wider. However, by allowing some bias (i.e as given by the region within the blue lines in each
+ ### graph) event the coarser scales can be investigated over a relatively larger temporal scale.
+ 
+ ### To observe which component dominates we need to observe the one that has the highest coefficients in absolute value.
+ 
+ 
+### MODWT of the ASV11
+ # Create an empty list to store your plots
+ plotList <- vector("list", n.levels)
+ 
+ for (i in 1:n.levels) { 
+   plot(as.vector(time(abund11)), modwtasv11_biased[[i]], xlab = "", ylab = "", type = "h", 
+        bty = "n", axes = F, ylim = range(modwtasv11[1:n.levels], na.rm = TRUE)) 
+   axis(2, cex.axis = 1.5, las = 2) +
+     abline(v = seq(start(abund11)[1], end(abund11)[1], by = 1), col = "gray", 
+            lty = "dashed") + abline(h = 0) 
+   abline(v = time(abund11)[range(which(!is.na(modwtasv11[[i]])))], lwd = 2, 
+          col = "red") + 
+     abline(v = time(abund11)[c(boundaries$start[i], boundaries$end[i])], col = "blue", 
+            lwd = 3, lty = "dashed") +
+     mtext(side = 4, line = -2, cex = 1.2, outer = FALSE, at = par("usr")[4], text = paste("d", i, sep = ""), las = 2) 
+   
+   # Save each plot to the list
+   plotList[[i]] <- recordPlot()
+ }
+ 
+ # Set the title for the entire series of plots
+ title(main = "ASV 11")
+ 
+ # Replay and display the saved plots
+ for (i in 1:n.levels) {
+   replayPlot(plotList[[i]])
+ }
+
+plot(as.vector(time(abund11)), modwtasv11_biased$s4, xlab = "", ylab = "", type = "h",
+     bty = "n", axes = F) > axis(1, cex.axis = 1.5) 
+axis(2, cex.axis = 1.5, las = 2) +
+mtext(side = 4, line = -2, cex = 1.2, outer = F, at = par("usr")[4], text = paste("s4", 
+                                                                                  sep = ""), las = 2) 
+title(main = "ASV 11")
+abline(v = time(abund11)[range(which(!is.na(modwtasv11[[5]])))], lwd = 2, 
+       col = "red") 
+abline(v = time(abund11)[c(boundaries$start[5], boundaries$end[5])], col = "blue", 
+       lwd = 3, lty = "dashed")
+
+##MODWT of the ASV178
+par(mfrow = c(n.levels + 1, 1), mar = c(2, 3, 0.5, 0.2))
+# Assuming n.levels is defined before this code snippet
+
+# Create an empty list to store your plots
+plotList <- vector("list", n.levels)
+
+for (i in 1:n.levels) { 
+  plot(as.vector(time(abund178)), modwtasv178_biased[[i]], xlab = "", ylab = "", type = "h", 
+       bty = "n", axes = F, ylim = range(modwtasv178[1:n.levels], na.rm = TRUE)) 
+  axis(2, cex.axis = 1.5, las = 2) +
+    abline(v = seq(start(abund178)[1], end(abund178)[1], by = 1), col = "gray", 
+           lty = "dashed") + abline(h = 0) 
+  abline(v = time(abund178)[range(which(!is.na(modwtasv178[[i]])))], lwd = 2, 
+         col = "red") + 
+    abline(v = time(abund178)[c(boundaries$start[i], boundaries$end[i])], col = "blue", 
+           lwd = 3, lty = "dashed") +
+    mtext(side = 4, line = -2, cex = 1.2, outer = FALSE, at = par("usr")[4], text = paste("d", i, sep = ""), las = 2) 
+  
+  # Save each plot to the list
+  plotList[[i]] <- recordPlot()
+}
+
+# Set the title for the entire series of plots
+title(main = "ASV 178")
+
+# Replay and display the saved plots
+for (i in 1:n.levels) {
+  replayPlot(plotList[[i]])
+}
+
+plot(as.vector(time(abund178)), modwtasv178_biased$s4, xlab = "", ylab = "", type = "h",
+     bty = "n", axes = F) > axis(1, cex.axis = 1.5) 
+axis(2, cex.axis = 1.5, las = 2) 
+mtext(side = 4, line = -2, cex = 1.2, outer = F, at = par("usr")[4], text = paste("s4", 
+                                                                                  sep = ""), las = 2) +
+abline(v = time(abund178)[range(which(!is.na(modwtasv178[[5]])))], lwd = 2, 
+       col = "red") 
+title(main = "ASV 178")
+abline(v = time(abund178)[c(boundaries$start[5], boundaries$end[5])], col = "blue", 
+       lwd = 3, lty = "dashed")
+
+##Gain function----
+#### No va la lliberia!!
+# install.packages('wmsta', repos = "https://CRAN.R-project.org/package=wmtsa")
+# library(wmtsa)
+# 
+# FreqResponsFunctionS8 <- wavGain(wavelet = "s8", n.levels = 4, n.fft = 1024, normalize = TRUE)
+# SqrGainHigh <- t(matrix(FreqResponsFunctionS8$sqrgain$high, nrow = 4, byrow = F)) 
+# colnames(SqrGainHigh) <- c("Level1", "Level2", "Level3", "Level4") 
+# SqrGainLow <- t(matrix(FreqResponsFunctionS8$sqrgain$low, nrow = 4, byrow = F)) 
+# colnames(SqrGainLow) <- c("Level1", "Level2", "Level3", "Level4") 
+# par(mar = c(3, 4, 1, 0.1)) 
+# plot(1:512, type = "n", axes = F, ylab = "Squared Gain Function", xlab = "", xlim = c(1, + 512), ylim = c(0, 1), font.lab = 1) 
+# polygon(x = c(1/16, 1/8, 1/8, 1/16) * 1024, y = c(-0.1, -0.1, 1.1, 1.1), col = "lightgray", 
+#         border = NA) 
+# polygon(x = c(1/4, 1/2, 1/2, 1/4) * 1024, y = c(-0.1, -0.1, 1.1, 1.1), col = "lightgray", 
+#         border = NA) 
+# polygon(x = c(0, 1/32, 1/32, 0) * 1024, y = c(-0.1, -0.1, 1.1, 1.1), col = "lightgray", 
+#         border = NA) 
+# box(bty = "L", lwd = 1) 
+# matplot(SqrGainHigh[1:512, ], type = "l", lty = "solid", axes = F, ylab = "Squared Gain", 
+#         xlab = "Scales [month]", add = T) > lines(1:512, SqrGainLow[1:512, 4], col = "blue") 
+# abline(v = c(1/24, 1/12, 1/6, 1/3) * 1024, lty = "solid", lwd = 1) 
+# axis(2, font.axis = 1.3, las = 2) 
+# mtext(side = 3, line = 0.25, outer = F, text = c(32, 16, 8, 4, 2), at = c(1/32, 1/16,  1/8, 1/4, 1/2) * 1024, font = 1, cex = 0.8) 
+# text(x = c(1/72, 1/24, 1/12, 1/6, 1/3) * 1024, y = rep(0.5, 4), labels = c("s4", "d4", + "d3", "d2", "d1"), font = 1, cex = 0.75, pos = 4, offset = 0.1) 
+# axis(1, font.axis = 1, at = c(1/24, 1/12, 1/6, 1/3) * 1024, labels = c(24, 12, 6, 3), 
+#      cex.axis = 1) 
+# mtext(side = 1, line = 1.6, outer = F, at = 256, text = "Periodicity (# obs.)", font = 1)
 
 
+## Apply Fourier analysis to compare-----
+par(fig = c(0, 0.48, 0.5, 1), mar = c(3, 5, 0, 0)) 
+plot((abund11), bty = "n", cex.axis = 1.3, ylab = "", xlab = "", axes = F) 
+axis(1, cex.axis = 1.3) 
+axis(2, cex.axis = 1.3, las = 2) 
+box(bty = "L", lwd = 1.1) 
+mtext(side = 2, line = 3.5, outer = F, text = expression("[N" * O[3]^"-" * "] (" * + mu * "mol/l)"), cex = 1.3) 
+par(new = T, fig = c(0, 0.24, 0, 0.5), mar = c(4, 5, 0, 0)) 
+spec.pgram(abund11, detrend = T, spans = c(5, 5), bty = "n", main = "", sub = "", 
+           xlab = "", ylab = "", demean = T, axes = F) 
+axis(1, cex.axis = 1.3) 
+axis(2, cex.axis = 1.3, las = 2) 
+box(bty = "L", lwd = 1.1) 
+mtext(side = 2, line = 3.5, outer = F, at = 0.15, text = "Periodogram", cex = 1.3) 
+mtext(side = 1, line = 2, at = 3, outer = F, text = expression("Freq. (year"^"-1" * + ")"), cex = 0.5) 
+abline(v = 1, col = "red") 
+par(new = T, fig = c(0.24, 0.48, 0, 0.5), mar = c(4, 5, 0, 0)) 
+acf(abund11, demean = T, ci.type = "white", bty = "n", main = "", sub = "", 
+    xlab = "", ylab = "", axes = F) 
+axis(1, cex.axis = 1.3) 
+axis(2, cex.axis = 1.3, las = 2) 
+box(bty = "L", lwd = 1.1) 
+mtext(side = 2, line = 3.5, outer = F, at = 0.25, text = "Autocorrelation", cex = 1.3) 
+mtext(side = 1, line = 2, at = 0.75, outer = F, text = "Time lag (year)", cex = 1.3) 
+
+Spline <- smooth.spline(seq(1995, 2004 + 11/12, by = 1/12), abund178, spar = 0.75)$y 
+par(new = T, fig = c(0.52, 1, 0.5, 1), mar = c(3, 5, 0, 0)) 
+plot(abund178, bty = "n", cex.lab = 1, cex.axis = 1, ylab = "", xlab = "", axes = F) 
+axis(1, cex.axis = 1.3) 
+axis(2, cex.axis = 1.3, las = 2) > box(bty = "L", lwd = 1.1) 
+mtext(side = 2, line = 3.5, outer = F, text = expression("[DON] (" * mu * "mol/l)"),  cex = 1.3) 
+lines(seq(1995, 2004 + 11/12, by = 1/12), Spline, col = "red")
+par(new = T, fig = c(0.52, 0.76, 0, 0.5), mar = c(4, 5, 0, 0)) 
+spec.pgram(abund178, detrend = T, spans = c(5, 5), bty = "n", main = "", sub = "", 
+           xlab = "", ylab = "", demean = T, axes = F) 
+axis(1, cex.axis = 1.3) 
+axis(2, cex.axis = 1.3, las = 2) 
+box(bty = "L", lwd = 1.1) 
+mtext(side = 2, line = 3.5, outer = F, at = 0.055, text = "Periodogram", cex = 1.3) 
+mtext(side = 1, line = 2, at = 3, outer = F, text = expression("Freq. (year"^"-1" * + ")"), cex = 1.3) 
+abline(v = 1, col = "red")
+
+par(new = T, fig = c(0.76, 1, 0, 0.5), mar = c(4, 5, 0, 0)) 
+acf(abund178 - Spline, demean = T, ci.type = "white", bty = "n", main = "", sub = "", 
+    xlab = "", ylab = "", axes = F) 
+axis(1, cex.axis = 1.3) 
+axis(2, cex.axis = 1.3, las = 2) 
+  box(bty = "L", lwd = 1.1) 
+  mtext(side = 2, line = 3.5, outer = F, at = 0.4, text = "Autocorrelation", cex = 1.3) 
+  mtext(side = 1, line = 2, at = 0.75, outer = F, text = "Time lag (year)", cex = 1.3)
+
+## code it in a more friendly way
+  library(ggplot2)
+  library(tidyr)
+  
+  ##trying to solve the error 
+  # Compute the periodogram
+  periodogram <- spec.pgram(abund178, detrend = TRUE, spans = c(5, 5),
+                            taper = 0.1, pad = 1)
+  
+  # Extract frequencies and corresponding spectral density values
+  frequencies <- periodogram$freq
+  spec_density <- periodogram$spec
+  
+  # Create a data frame for plotting
+  periodogram_df <- data.frame(frequencies, spec_density)
+  
+  sampling_freq <- asv178$decimal_date |>
+    as_tibble_col(column_name = "real_sampling") 
+  
+  perfect_sampling_freq<- seq(2004, 2013 + 11/12, by = 1/12)|>
+    as_tibble_col(column_name = 'ideal_sampling') 
+  
+
+  
+  # Data preparation
+  asv11$decimal_date
+  abund178_df <- data.frame(year = asv178$decimal_date, #seq(2004, 2013 + 11/12, by = 1/12),
+                            abund178 = abund178,
+                            Spline = Spline,
+                            abund178_minus_Spline = abund178 - Spline)
+  
+  acf_df <- acf(abund178_df$abund178_minus_Spline, demean = TRUE)$acf |>
+    as_tibble() |>
+    dplyr::mutate(n = row_number())
+  
+  # Plotting using ggplot2
+  plots <- list(
+    ggplot(abund178_df, aes(x = year, y = abund178)) +
+      geom_line(color = "black") +
+      theme_minimal() +
+      labs(x = NULL, y = expression("[N" * O[3]^"-" * "] (" * mu * "mol/l)")),
+    
+    # Plot the periodogram
+    ggplot(periodogram_df, aes(x = frequencies, y = spec_density)) +
+      geom_line(color = "black") +
+      theme_minimal() +
+      labs(x = "Frequency", y = "Spectral Density", title = "Spectral Analysis") +
+      geom_vline(xintercept = 1, linetype = "dashed", color = "red"),
+    
+    ggplot(acf_df, aes(n, y = V1)) +
+      geom_line(color = "black") +
+      geom_col()+
+      theme_minimal() +
+      labs(x = "Time lag (year)", y = "Autocorrelation", title = "Autocorrelation Analysis") +
+      geom_hline(yintercept = 0, linetype = "dashed", color = "red")
+  )
+  
+  # Arrange and display the plots
+  do.call(gridExtra::grid.arrange, c(plots, ncol = 3))
+  
+  
+
+  
