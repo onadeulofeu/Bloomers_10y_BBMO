e32d19d35402ab497174093662c97e958ebe7617
onadeulofeu <ona.deulofeu@gmail.com>
2024-02-14 14:28:03 +0100

    add information to the phylogenetic tree

--- scripts/phylogenetic_tree.R
+++ scripts/phylogenetic_tree.R
@@ -33,7 +33,7 @@ close(file_conn)
 # Print a message indicating the file has been written
 cat("FASTA file '", output_file, "' has been created.\n")
 
-## Después corremos este trozo de código en marbits o en local look at README file in the folder----
+## Run thi code in marbits or in local look at README file in the folder----
 
 # module load
 # 
@@ -52,9 +52,11 @@ cat("FASTA file '", output_file, "' has been created.\n")
 # raxml-ng --support --tree remei100.raxml.bestTree --bs-trees remei100.raxml.bootstraps --prefix remei100 --threads 2 ## para juntar las bootstraps con el tree
 
 
-## Visualization of our tree
+## Visualization of our tree----
 #devtools::install_github("GuangchuangYu/ggtree")
 library(ggtree)
+library(ggplot)
+library(tidyverse)
 
 tree <- ggtree::read.tree('data/raxml/bloo_bbmo.raxml.support')
 
@@ -65,4 +67,92 @@ tree |>
   #geom_treescale()+
   geom_tiplab(align = T)
   #geom_tippoint()
-  
+
+# I try to add a column with information of the types of blooms that they form (from the wavelets analysis). 
+
+
+# And another column with the information of in which fraction do they bloom.
+tree |>
+  ggplot() + 
+  geom_tree() + 
+  theme_tree2() +
+  #geom_treescale()+
+  geom_tiplab(align = T)
+
+tax <- asv_tab_all_bloo_z_tax |>
+  dplyr::select(asv_num, phylum, class, order, family, genus) |>
+  distinct()
+
+metadata <- bloo_02 |>
+  dplyr::mutate(fraction = '0.2') |>
+  bind_rows(bloo_3) |>
+  dplyr::mutate(fraction = case_when(is.na(fraction)~ '3',
+                                     !is.na(fraction) ~ fraction)) |>
+  group_by(value) |>
+  dplyr::mutate(detect = n()) |>
+  dplyr::mutate(detect_both = case_when(detect == '2' ~ 'both',
+                                        detect == '1' ~ fraction)) |>
+  group_by( value) |>
+  dplyr::select(detect_both, value) |>
+  distinct() |>
+  rename(asv_num = value) |>
+  left_join(tax)
+
+merged_data <- merge(as_tibble_col(tree$tip.label, column_name = 'asv_num'), metadata, by = "asv_num") |>
+  dplyr::mutate(family_num = paste0(family,' ', asv_num))
+
+ tree_plot <- ggtree(tree, layout='dendrogram', branch.length='none') %<+% ## this is used to assign new data to the plot
+  merged_data +
+  geom_tree(aes(color=class))+
+  scale_color_manual(values = palette_class_assigned)+
+  geom_tiplab( aes(label=family), size=2, align=TRUE) +
+    labs(color = 'Class')+
+  #geom_treescale()+
+  #geom_tiplab(align = T)+
+  theme_tree2()
+  #geom_treescale(x = 10, y = 360)
+ 
+ ## prepare the data if they are blooming on the FL or the PA fraction
+ detect_both <- merged_data |>
+   dplyr::select(detect_both, asv_num) |>
+   dplyr::mutate(blooming_fraction = 1,
+                 detect_both = str_replace(detect_both, '3', 'PA')) |>
+   dplyr::mutate(detect_both = str_replace(detect_both, '0.2', 'FL')) |>
+   pivot_wider(id_cols = asv_num, values_fill = 0, names_from = detect_both, values_from = blooming_fraction)
+ 
+ detect_both <- detect_both |>
+   dplyr::mutate_all(as.character) |>
+   dplyr::mutate(PA = case_when(both == '1' ~ '1',
+                                both == '0' ~ PA),
+                 FL =  case_when(both == '1' ~ '1',
+                                 both == '0' ~ FL)) |>
+   dplyr::select(-both)   |>
+   column_to_rownames(var = 'asv_num') 
+ 
+ palette_fraction <- c(  '0' = 'white', '1' = 'black' )
+
+ # Plot the dendrogram with colored branches based on 'class'
+ tree_plot <- ggtree(tree, branch.length='none') %<+% ## this is used to assign new data to the plot
+  merged_data +
+  geom_tree() + #aes(color=class)
+  #scale_color_manual(values = palette_class_assigned) +
+   geom_tiplab(aes(label = family_num), size=2, align=TRUE) + 
+  labs(color = 'Class') +
+  theme_tree2()+
+   theme(legend.position = "none",  # Remove legend
+         panel.grid.major = element_blank(),  # Remove grid lines
+         panel.grid.minor = element_blank(),  # Remove grid lines
+         axis.text.x = element_blank(),
+         axis.text.y = element_blank(),
+         axis.line.x = element_blank(),
+         axis.ticks.x = element_blank()) 
+
+
+# Create the heatmap colored by 'detect_both' variable
+heatmap <- gheatmap(tree_plot, detect_both, offset=7, width = .1, color=NA, font.size = 2) + 
+  scale_fill_manual(values = palette_fraction, name="Potential\nblooming\nin fraction", labels=c("0" = "No", "1" = "Yes"))+
+  theme(legend.position = 'bottom', text = element_text(size = 4),
+        legend.key.size = unit(0.5, "lines"),
+        )
+
+print(heatmap)
