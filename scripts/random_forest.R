ee30c6769acdecf191a62e545d564093b1183a29
onadeulofeu <ona.deulofeu@gmail.com>
2024-02-19 15:32:57 +0100

    script to perform the random forest analysis to try to identify which environmental factors are triggering these bloming events

---
+++ scripts/random_forest.R
@@ -0,0 +1,22 @@
+#packages----
+library(caret)
+
+## we need to create a wide dataset with environmental variables and each ASV that we consider it is a potential bloomer
+## do we need to normalize environmental variables to z-scores?
+## Is there a way to add the community effect on a blooming event in this test? add bray curtis as an env variable?
+## Could we add eukaryotes data here?
+
+
+## Caret package https://topepo.github.io/caret/index.html
+
+## prepare the dataset----
+
+
+
+
+### I pick representatives of different clusters and see how it goes
+
+### asv11 (Glaciecola)
+
+
+## ideas data that I should add here: diatoms, ciliates, dinophlgellates, 
