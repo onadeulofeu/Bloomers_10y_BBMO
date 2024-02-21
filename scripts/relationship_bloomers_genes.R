396eed4f85f0f9b43a20a6f580fcd4dd287ca4f3
onadeulofeu <ona.deulofeu@gmail.com>
2024-02-21 16:17:07 +0100

    update the code and nicer graphs

--- scripts/relationship_bloomers_genes.R
+++ scripts/relationship_bloomers_genes.R
@@ -1,3 +1,12 @@
+
+## edit metadata sample_id column so that it matches the sample_id code that we have in the genes data
+m_bbmo_10y |>
+  colnames()
+
+m_bbmo_10y_ed <- m_bbmo_10y |>
+    separate(sample_id, into = c('sample_id_sim', 'fraction', 'code'), sep = '_') |>
+    dplyr::filter(fraction == '0.2')
+
 #upload data from metagenomes we have the table with counts for the genes
 genes <- read.csv2('data/genes_sergio/K02692_genes_detail.csv') |>
   dplyr::select(annot, starts_with('BL')) |> #filter only Blanes samples not SOLA 
@@ -13,10 +22,14 @@ genes_sample_id <- genes %$%
   sample_id |>
   unique() #I have 56 samples in common with metaG data
 
-m_02_f <-m_bbmo_10y_ed |>
+m_bbmo_10y_ed_red <- m_bbmo_10y_ed |>
+  dplyr::filter(year %in% c('2009', '2010', '2011', '2012', '2013')) |>
+  dplyr::filter(sample_id_sim %in% genes_sample_id)
+
+m_02_f <- m_bbmo_10y_ed |>
   dplyr::filter(year %in% c('2009', '2010', '2011', '2012', '2013')) |>
   dplyr::filter(sample_id_sim %in% genes_sample_id) |>
-  dplyr::mutate(sample_id_num = str_c(1:nrow(m_02_f)))
+  dplyr::mutate(sample_id_num = str_c(1:nrow(m_bbmo_10y_ed_red)))
 
 m_02_f$sample_id_num
 
@@ -160,16 +173,19 @@ genes_counts_z_scores |>
   
 ## potential bloomers dataset for the same dates as we have metagenomes 2009-2013----
 ##blooming events 0.2 between 2009-2013 comparison with Metagenomes
+asv_tab_all_bloo_z_tax |>
+  colnames()
+
 abund_asv_bloom_events_02 <- asv_tab_all_bloo_z_tax |>
   dplyr::filter(fraction == '0.2') |>
   dplyr::filter(abundance_type == 'relative_abundance' &
-                  z_score_ra >= 1.96 &
-                  abundance_value >= 0.1) |> #we add this line in case we want potential bloomers not just anomalies in their rel abund
+                  as.numeric(z_score_ra) >= 1.96 &
+                  as.numeric(abundance_value) >= 0.1) |> #we add this line in case we want potential bloomers not just anomalies in their rel abund
   dplyr::select(year, sample_id, asv_num, abundance_type, abundance_value) |>
   dplyr::filter(year %in% c('2009', '2010', '2011', '2012', '2013')) |>
   left_join(tax_bbmo_10y_new)
 
-#write.csv(abund_asv_bloom_events_02, 'data/abund_asv_bloom_events_02.csv')
+write.csv(abund_asv_bloom_events_02, 'data/genes_sergio/abund_asv_bloom_events_02.csv')
 
 ## plot them to see if there's some kind of correlation with KEGGs
 abund_asv_bloom_events_02_unique <- asv_tab_all_bloo_z_tax |>
@@ -182,13 +198,22 @@ abund_asv_bloom_events_02_unique <- asv_tab_all_bloo_z_tax |>
   left_join(tax_bbmo_10y_new) |>
   distinct(asv_num)
 
-#write.csv(abund_asv_bloo_fl_09_13, 'data/abund_asv_bloo_fl_09_13.csv')
+abund_asv_bloo_fl_09_13 <- asv_tab_all_bloo_z_tax |>
+  dplyr::filter(fraction == '0.2') |>
+  dplyr::filter(abundance_type == 'relative_abundance' &
+                  z_score_ra >= 1.96 &
+                  abundance_value >= 0.1) |> #we add this line in case we want potential bloomers not just anomalies in their rel abund
+  dplyr::select(year, sample_id, asv_num, abundance_type, abundance_value, date) |>
+  dplyr::filter(year %in% c('2009', '2010', '2011', '2012', '2013')) |>
+  left_join(tax_bbmo_10y_new)
 
-asv_tab_all_bloo_z_tax |>
+#write.csv(abund_asv_bloo_fl_09_13, 'data/genes_sergio/abund_asv_bloo_fl_09_13.csv')
+
+bloo_02_09_13_plot <- asv_tab_all_bloo_z_tax |>
   dplyr::filter(fraction == '0.2') |>
   dplyr::filter(year %in% c('2009', '2010', '2011', '2012', '2013')) |>
   dplyr::filter(asv_num %in% abund_asv_bloom_events_02_unique$asv_num) |>
-  dplyr::filter(abundance_type == 'pseudoabundance') |>
+  dplyr::filter(abundance_type == 'relative_abundance') |>
   ggplot(aes(date, abundance_value))+
   facet_wrap(vars(asv_num))+
   scale_x_datetime(expand = c(0,0),
@@ -197,15 +222,23 @@ asv_tab_all_bloo_z_tax |>
                    date_labels = "%Y")+
   #geom_hline(yintercept = 0.1, color = '#8B8989')+
   geom_line(aes(group = asv_num), color = '#5B5A5A')+ #, color = '#3D3B3B'
-  geom_point(data = asv_tab_bloo_pseudo_anom |>
+  geom_point(data = abund_asv_bloo_fl_09_13 |>
                dplyr::filter(year %in% c('2009', '2010', '2011', '2012', '2013') &
                                asv_num %in% abund_asv_bloom_events_02_unique$asv_num),
-             aes(date, abundance_value, color =  '#9F0011', alpha = 1), size = 1)+
+             aes(date, abundance_value, color =  '#9F0011'), size = 1)+
+  scale_y_continuous(labels = percent_format())+
+  geom_area()+
+  labs(colour = '', alpha = '', y = 'Relative abundance (%)', x = 'Date')+
   theme_bw()+
   guides(guide_legend(colour = 'none',
                       alpha = 'none'))+
   theme(panel.grid = element_blank(), text = element_text(size = 7), panel.border = element_blank(),
-        axis.title.x = element_blank(), axis.ticks.x = element_blank(), aspect.ratio = 3/7)
+       axis.ticks.x = element_blank(), aspect.ratio = 3/7,
+        strip.background = element_blank(), legend.position = 'none')
+
+ggsave( plot = bloo_02_09_13_plot, filename = 'bloo_02_09_13_plot.pdf',
+        path = 'results/figures/relationship_genes_blooms/',
+        width = 180, height = 160, units = 'mm')
 
 max_abund_asv_bloom_events |>
   dim() == samples_with_bloom_event |>
@@ -214,24 +247,24 @@ max_abund_asv_bloom_events |>
 samples_with_bloom_event_asv_abund <- samples_with_bloom_event |>
   right_join(max_abund_asv_bloom_events)
 
-## relationship between genes and blooming ASVs.----
+## relationship between genes and blooming ASVs----
 ### I need the to compare normalized genes and normalized ASVs abundances.
-zclr_bloo_fl_09_13 <- asv_tab_all_bloo_z_tax |>
+rclr_bloo_fl_09_13 <- asv_tab_all_bloo_z_tax |>
   dplyr::filter(fraction == '0.2') |>
   dplyr::filter(year %in% c('2009', '2010', '2011', '2012', '2013')) |>
   dplyr::filter(asv_num %in% abund_asv_bloom_events_02_unique$asv_num) |>
-  dplyr::filter(abundance_type == 'zclr')
+  dplyr::filter(abundance_type == 'rclr')
 
 genes_counts_z_scores_f <- genes_counts_z_scores |>  
   group_by(annot) |>
-  dplyr::filter(any(gene_counts > 5)) ## I do it for the most abundant genes to simplify
-
-
-zscores_gen_ra_fl_09_13 <- zclr_bloo_fl_09_13 |>
-  left_join(genes_counts_z_scores_f, by = c('date' = 'date.y')) 
+  dplyr::filter(any(gene_counts > 5)) |> ## I do it for the most abundant genes to simplify
+dplyr::select(-date.y)
+  
+zscores_gen_ra_fl_09_13 <- rclr_bloo_fl_09_13 |>
+  left_join(genes_counts_z_scores_f, by = c('date' = 'date.x'), relationship = 'many-to-many') 
 
 zscores_gen_ra_fl_09_13 |>
-  dplyr::filter(!is.na(annot)) |>
+  #dplyr::filter(!is.na(annot)) |>
   ggplot(aes(z_score_ra, z_score_gen))+
   geom_point()+
   geom_hline(yintercept = 0)+
@@ -245,8 +278,8 @@ zclr_bloo_fl_09_13_sim <- zclr_bloo_fl_09_13 |>
   rename(z_score = z_score_ra, variable = asv_num)
 
 genes_counts_z_scores_f_sim <- genes_counts_z_scores_f |>
-  dplyr::select(date.y, z_score_gen, annot) |>
-  rename(date = date.y, z_score = z_score_gen, variable = annot)
+  dplyr::select(date.x, z_score_gen, annot) |>
+  rename(date = date.x, z_score = z_score_gen, variable = annot)
 
 zclr_bloo_fl_09_13_sim |>
   bind_rows(genes_counts_z_scores_f_sim) |>
